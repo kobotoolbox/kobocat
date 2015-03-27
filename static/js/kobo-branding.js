e6e9875546e5e5666415856d4f2e34ad5e1d4bfe
pmusaraj <pmusaraj@gmail.com>
2015-03-27 13:47:51 -0400

    vex fixes + improvements to responsiveness of KC

--- static/js/kobo-branding.js
+++ static/js/kobo-branding.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
   $(document).on("click", ".header-bar__top-level-menu-button", function () {
     $('.top-level-menu').toggleClass('is-active');
   });
+
+  $('table.published_forms__table').footable();
 }); 
