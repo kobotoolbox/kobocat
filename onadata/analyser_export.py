6b37f247be4256255fcb5628fe9ec5909406bcc9
Esmail <esmail.fadae@kobotoolbox.org>
2015-05-31 13:46:29 -0400

    Excel analyser exports working.

--- onadata/analyser_export.py
+++ onadata/analyser_export.py
@@ -5,28 +5,44 @@ Created on May 14, 2015
 '''
 
 
-import zipfile
 import tempfile
 import io
 import shutil
 import copy
+from zipfile import (
+    ZipFile,
+    ZIP_DEFLATED,
+)
 
 import lxml.etree as etree
+import xlrd
+import openpyxl
+from openpyxl.writer.excel import save_virtual_workbook
 
 
 NAMESPACES= {'xmlns': 'http://schemas.openxmlformats.org/spreadsheetml/2006/main'}
 
 
-def insert_xlsform_worksheets(analyser_shared_strings, survey_worksheet_file_path, choices_worksheet_file_path, survey_file_xls):
-    raise NotImplementedError
+# Adapted from http://stackoverflow.com/a/9919409/1877326
+def xls_as_xlsx(xls_file):
+    # first open using xlrd
+    source_workbook = xlrd.open_workbook(file_contents=xls_file.read())
 
-# def fast_iter(context, func):
-#     for event, elem in context:
-#         func(elem)
-#         elem.clear()
-#         while elem.getprevious() is not None:
-#             del elem.getparent()[0]
-#     del context
+    # Create the destination workbook, deleting and auto-generated worksheets.
+    destination_workbook = openpyxl.Workbook() # TODO: Would like to figure out how to make appends work with a "write_only" workbook.
+    for wksht_nm in destination_workbook.sheetnames:
+        worksheet= destination_workbook.get_sheet_by_name(wksht_nm)
+        destination_workbook.remove_sheet(worksheet)
+
+    worksheet_names= ['survey', 'choices']
+    for wksht_nm in worksheet_names:
+        source_worksheet= source_workbook.sheet_by_name(wksht_nm)
+        destination_worksheet= destination_workbook.create_sheet(title=wksht_nm)
+
+        for row in xrange(source_worksheet.nrows):
+            destination_worksheet.append( (source_worksheet.cell_value(row, col) for col in xrange(source_worksheet.ncols)) )
+
+    return io.BytesIO(save_virtual_workbook(destination_workbook))
 
 def copy_cells(source_worksheet_file, destination_worksheet_file_path, new_string_indices):
     destination_worksheet= etree.parse(destination_worksheet_file_path)
@@ -35,7 +51,7 @@ def copy_cells(source_worksheet_file, destination_worksheet_file_path, new_strin
     for _, source_dimension in etree.iterparse(source_worksheet_file, tag=namespace_prefix+'dimension'):
         destination_dimension= destination_worksheet.xpath('.//xmlns:dimension', namespaces=NAMESPACES)[0]
         destination_dimension.attrib['ref']= source_dimension.attrib['ref']
-        source_dimension.clear() # FIXME: Necessary? Enough?
+        source_dimension.getroottree().getroot().clear() # FIXME: Necessary? Enough?
         break
     else:
         raise ValueError('No "dimension" element found in source data file.')
@@ -54,8 +70,10 @@ def copy_cells(source_worksheet_file, destination_worksheet_file_path, new_strin
             destination_c= copy.deepcopy(source_c)
             # Remap references to shared strings.
             if destination_c.attrib['t'] == 's':
-                destination_v= destination_c.xpath('.//xmlns:v', namespaces=NAMESPACES)[0]
-                destination_v.text= unicode(new_string_indices[int(destination_v.text)])
+                values= destination_c.xpath('.//xmlns:v', namespaces=NAMESPACES)
+                if values:
+                    destination_v= values[0]
+                    destination_v.text= unicode(new_string_indices[int(destination_v.text)])
             destination_row.append(destination_c)
 
         # Clean up elements of the source worksheet to save memory.
@@ -68,10 +86,10 @@ def copy_cells(source_worksheet_file, destination_worksheet_file_path, new_strin
         destination_sheetData_element.append(destination_row)
 
     # Save the changes.
-    with open(destination_worksheet_file_path, 'wb') as destination_worksheet_file:
+    with open(destination_worksheet_file_path, 'w') as destination_worksheet_file:
         destination_worksheet.write(destination_worksheet_file, encoding='UTF-8')
 
-def insert_shared_strings(source_file, destination_etree, new_string_indices):
+def splice_shared_strings(source_file, destination_etree, new_string_indices):
     source_etree= etree.parse(source_file)
     original_string_map= dict()
     destination_root= destination_etree.getroot()
@@ -93,14 +111,38 @@ def insert_shared_strings(source_file, destination_etree, new_string_indices):
     # Update the "uniqueCount" attribute.
     destination_root.attrib['uniqueCount']= unicode(uniqueCount)
 
+def insert_xlsform_worksheets(analyser_shared_strings, analyser_survey_worksheet_file_path, analyser_choices_worksheet_file_path, survey_file_xls):
+    # Create an XLSX copy of the survey file.
+    survey_file_xlsx= xls_as_xlsx(survey_file_xls)
+
+    with ZipFile(survey_file_xlsx) as survey_zipfile:
+        # Splice over the shared strings.
+        with survey_zipfile.open('xl/sharedStrings.xml') as shared_strings_file:
+            new_string_indices= dict()
+            splice_shared_strings(shared_strings_file, analyser_shared_strings, new_string_indices)
+        # Copy over the "survey" sheet.
+        with survey_zipfile.open('xl/worksheets/sheet1.xml') as source_survey_worksheet_file:
+            # Create a tempfile that supports seeking.
+            with tempfile.TemporaryFile('w+') as source_survey_worksheet_tempfile:
+                source_survey_worksheet_tempfile.write(source_survey_worksheet_file.read())
+                source_survey_worksheet_tempfile.seek(0)
+                copy_cells(source_survey_worksheet_tempfile, analyser_survey_worksheet_file_path, new_string_indices)
+        # Copy over the "choices" sheet.
+        with survey_zipfile.open('xl/worksheets/sheet2.xml') as source_choices_worksheet_file:
+            # Create a tempfile that supports seeking.
+            with tempfile.TemporaryFile('w+') as source_choices_worksheet_tempfile:
+                source_choices_worksheet_tempfile.write(source_choices_worksheet_file.read())
+                source_choices_worksheet_tempfile.seek(0)
+                copy_cells(source_choices_worksheet_tempfile, analyser_choices_worksheet_file_path, new_string_indices)
+
 def insert_data_sheet(analyser_shared_strings, analyser_data_sheet_file_path, data_file_xlsx):
-    with zipfile.ZipFile(data_file_xlsx) as data_zipfile:
+    with ZipFile(data_file_xlsx) as data_zipfile:
         with data_zipfile.open('xl/sharedStrings.xml') as shared_strings_file:
             new_string_indices= dict()
-            insert_shared_strings(shared_strings_file, analyser_shared_strings, new_string_indices)
+            splice_shared_strings(shared_strings_file, analyser_shared_strings, new_string_indices)
         # FIXME: Not safe for multi-sheet data such for repeating groups.
         with data_zipfile.open('xl/worksheets/sheet1.xml') as data_worksheet_file:
-            with tempfile.TemporaryFile('wb+') as data_worksheet_tempfile:
+            with tempfile.TemporaryFile('w+') as data_worksheet_tempfile:
                 data_worksheet_tempfile.write(data_worksheet_file.read())
                 data_worksheet_tempfile.seek(0)
                 copy_cells(data_worksheet_tempfile, analyser_data_sheet_file_path, new_string_indices)
@@ -108,31 +150,33 @@ def insert_data_sheet(analyser_shared_strings, analyser_data_sheet_file_path, da
 def generate_analyser(analyser_file_xlsx, data_file_xlsx, survey_file_xls):
     # Unzip the analyser in preparation for modifications.
     temp_directory_path= tempfile.mkdtemp(prefix='analyser_temp_')
-    with zipfile.ZipFile(analyser_file_xlsx) as analyser_zipfile:
-        analyser_zipfile.extractall(temp_directory_path)
-        zip_contents= [f.filename for f in analyser_zipfile.filelist]
+    try:
+        with ZipFile(analyser_file_xlsx) as analyser_zipfile:
+            analyser_zipfile.extractall(temp_directory_path)
+            zip_contents= [f.filename for f in analyser_zipfile.filelist]
 
-    analyser_shared_strings= etree.parse(temp_directory_path + '/xl/sharedStrings.xml')
+        analyser_shared_strings= etree.parse(temp_directory_path + '/xl/sharedStrings.xml')
 
-    # Copy the data over to the analyser.
-    analyser_data_sheet_file_path= temp_directory_path + '/xl/worksheets/sheet8.xml'
-    insert_data_sheet(analyser_shared_strings, analyser_data_sheet_file_path, data_file_xlsx)
+        # Copy over the XLSForm's "survey" and "choices" (if present) worksheets.
+        survey_worksheet_file_path= temp_directory_path + '/xl/worksheets/sheet9.xml'
+        choices_worksheet_file_path= temp_directory_path + '/xl/worksheets/sheet10.xml'
+        insert_xlsform_worksheets(analyser_shared_strings, survey_worksheet_file_path, choices_worksheet_file_path, survey_file_xls)
 
-    # Copy over the XLSForm's "survey" and "choices" (if present) worksheets.
-    survey_worksheet_file_path= temp_directory_path + '/xl/worksheets/sheet9.xml'
-    choices_worksheet_file_path= temp_directory_path + '/xl/worksheets/sheet10.xml'
-    insert_xlsform_worksheets(analyser_shared_strings, survey_worksheet_file_path, choices_worksheet_file_path, survey_file_xls)
+        # Copy the data over to the analyser.
+        analyser_data_sheet_file_path= temp_directory_path + '/xl/worksheets/sheet8.xml'
+        insert_data_sheet(analyser_shared_strings, analyser_data_sheet_file_path, data_file_xlsx)
 
-    # Finalize the changes for export.
-    with open(temp_directory_path + '/xl/sharedStrings.xml', 'wb') as analyser_shared_file:
-        analyser_shared_strings.write(analyser_shared_file, encoding='UTF-8')
-    xlsx_out= io.BytesIO()
-    with zipfile.ZipFile(xlsx_out, 'w', compression=zipfile.ZIP_DEFLATED) as zipfile_out:
-        for file_path in zip_contents:
-            zipfile_out.write(temp_directory_path + '/' + file_path, file_path)
+        # Finalize the changes for export.
+        with open(temp_directory_path + '/xl/sharedStrings.xml', 'wb') as analyser_shared_strings_file:
+            analyser_shared_strings.write(analyser_shared_strings_file, encoding='UTF-8')
+        xlsx_out= io.BytesIO()
+        with ZipFile(xlsx_out, 'w', compression=ZIP_DEFLATED) as zipfile_out:
+            for file_path in zip_contents:
+                zipfile_out.write(temp_directory_path + '/' + file_path, file_path)
 
-    # Clean up temporary files.
-    shutil.rmtree(temp_directory_path)
+    finally:
+        # Clean up temporary files.
+        shutil.rmtree(temp_directory_path)
 
     xlsx_out.seek(0)
     return xlsx_out
@@ -140,6 +184,7 @@ def generate_analyser(analyser_file_xlsx, data_file_xlsx, survey_file_xls):
 if __name__ == '__main__':
     analyser_file_xlsx= '/home/esmail/Downloads/KoBoToolbox_Excel_Data_Analyser_1.23_TEMPLATE.xlsx'
     data_file_xlsx= '/home/esmail/Downloads/uga_14_v6_2015_03_02_14_34_38.xlsx'
-    xlsx_out= generate_analyser(analyser_file_xlsx, data_file_xlsx, None)
-    with open('/home/esmail/Downloads/data_inserted.xlsx', 'wb') as f:
+    survey_file= open('/home/esmail/Downloads/uga_14_v6.xls')
+    xlsx_out= generate_analyser(analyser_file_xlsx, data_file_xlsx, survey_file)
+    with open('/home/esmail/Downloads/generated_analyser.xlsx', 'wb') as f:
         f.write(xlsx_out.read())
