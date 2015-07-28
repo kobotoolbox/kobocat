8b63233325df1eeaf4fdc5f3677e7fa37dcad073
Ben Pitzer <pitzer@google.com>
2015-07-28 13:04:09 +0545

    Adds a base class for export builders and a first implementation of a Sheets builder.

---
+++ onadata/libs/utils/export_builder.py
@@ -0,0 +1,318 @@
+
+from datetime import datetime
+from pyxform.question import Question
+from pyxform.section import Section, RepeatingSection
+
+from onadata.apps.viewer.models.parsed_instance import\
+    _is_invalid_for_mongo, _encode_for_mongo, _decode_from_mongo
+from onadata.libs.utils.common_tags import (
+    ID, XFORM_ID_STRING, STATUS, ATTACHMENTS, GEOLOCATION, BAMBOO_DATASET_ID,
+    DELETEDAT, USERFORM_ID, INDEX, PARENT_INDEX, PARENT_TABLE_NAME,
+    SUBMISSION_TIME, UUID, TAGS, NOTES)
+
+QUESTION_TYPES_TO_EXCLUDE = [
+    u'note',
+]
+# the bind type of select multiples that we use to compare
+MULTIPLE_SELECT_BIND_TYPE = u"select"
+GEOPOINT_BIND_TYPE = u"geopoint"
+
+
+class ExportBuilder(object):
+    """A base class for export builders."""
+    
+    IGNORED_COLUMNS = [XFORM_ID_STRING, STATUS, ATTACHMENTS, GEOLOCATION,
+                       BAMBOO_DATASET_ID, DELETEDAT]
+    # fields we export but are not within the form's structure
+    EXTRA_FIELDS = [ID, UUID, SUBMISSION_TIME, INDEX, PARENT_TABLE_NAME,
+                    PARENT_INDEX, TAGS, NOTES]
+    SPLIT_SELECT_MULTIPLES = True
+    BINARY_SELECT_MULTIPLES = False
+    
+    # column group delimiters
+    GROUP_DELIMITER_SLASH = '/'
+    GROUP_DELIMITER_DOT = '.'
+    GROUP_DELIMITER = GROUP_DELIMITER_SLASH
+    GROUP_DELIMITERS = [GROUP_DELIMITER_SLASH, GROUP_DELIMITER_DOT]
+    TYPES_TO_CONVERT = ['int', 'decimal', 'date']  # , 'dateTime']
+    CONVERT_FUNCS = {
+        'int': lambda x: int(x),
+        'decimal': lambda x: float(x),
+        'date': lambda x: datetime.strptime(x, "%d%m%Y").date(),
+        'dateTime': lambda x: datetime.strptime(x[:19], '%Y-%m-%dT%H:%M:%S')
+    }
+    SHEET_NAME_MAX_CHARS = 31
+
+    def export(self, path, data, *args):
+        raise NotImplementedError
+        
+    def set_survey(self, survey):
+        # TODO resolve circular import
+        from onadata.apps.viewer.models.data_dictionary import\
+            DataDictionary
+
+        def build_sections(
+                current_section, survey_element, sections, select_multiples,
+                gps_fields, encoded_fields, field_delimiter='/'):
+            for child in survey_element.children:
+                current_section_name = current_section['name']
+                # if a section, recurs
+                if isinstance(child, Section):
+                    # if its repeating, build a new section
+                    if isinstance(child, RepeatingSection):
+                        # section_name in recursive call changes
+                        section = {
+                            'name': child.get_abbreviated_xpath(),
+                            'elements': []}
+                        self.sections.append(section)
+                        build_sections(
+                            section, child, sections, select_multiples,
+                            gps_fields, encoded_fields, field_delimiter)
+                    else:
+                        # its a group, recurs using the same section
+                        build_sections(
+                            current_section, child, sections, select_multiples,
+                            gps_fields, encoded_fields, field_delimiter)
+                elif isinstance(child, Question) and child.bind.get(u"type")\
+                        not in QUESTION_TYPES_TO_EXCLUDE:
+                    # add to survey_sections
+                    if isinstance(child, Question):
+                        child_xpath = child.get_abbreviated_xpath()
+                        current_section['elements'].append({
+                            'title': ExportBuilder.format_field_title(
+                                child.get_abbreviated_xpath(),
+                                field_delimiter),
+                            'xpath': child_xpath,
+                            'type': child.bind.get(u"type")
+                        })
+
+                        if _is_invalid_for_mongo(child_xpath):
+                            if current_section_name not in encoded_fields:
+                                encoded_fields[current_section_name] = {}
+                            encoded_fields[current_section_name].update(
+                                {child_xpath: _encode_for_mongo(child_xpath)})
+
+                    # if its a select multiple, make columns out of its choices
+                    if child.bind.get(u"type") == MULTIPLE_SELECT_BIND_TYPE\
+                            and self.SPLIT_SELECT_MULTIPLES:
+                        for c in child.children:
+                            _xpath = c.get_abbreviated_xpath()
+                            _title = ExportBuilder.format_field_title(
+                                _xpath, field_delimiter)
+                            choice = {
+                                'title': _title,
+                                'xpath': _xpath,
+                                'type': 'string'
+                            }
+
+                            if choice not in current_section['elements']:
+                                current_section['elements'].append(choice)
+                        _append_xpaths_to_section(
+                            current_section_name, select_multiples,
+                            child.get_abbreviated_xpath(),
+                            [c.get_abbreviated_xpath()
+                             for c in child.children])
+
+                    # split gps fields within this section
+                    if child.bind.get(u"type") == GEOPOINT_BIND_TYPE:
+                        # add columns for geopoint components
+                        xpaths = DataDictionary.get_additional_geopoint_xpaths(
+                            child.get_abbreviated_xpath())
+                        current_section['elements'].extend(
+                            [
+                                {
+                                    'title': ExportBuilder.format_field_title(
+                                        xpath, field_delimiter),
+                                    'xpath': xpath,
+                                    'type': 'decimal'
+                                }
+                                for xpath in xpaths
+                            ])
+                        _append_xpaths_to_section(
+                            current_section_name, gps_fields,
+                            child.get_abbreviated_xpath(), xpaths)
+
+        def _append_xpaths_to_section(current_section_name, field_list, xpath,
+                                      xpaths):
+            if current_section_name not in field_list:
+                field_list[current_section_name] = {}
+            field_list[
+                current_section_name][xpath] = xpaths
+
+        self.survey = survey
+        self.select_multiples = {}
+        self.gps_fields = {}
+        self.encoded_fields = {}
+        main_section = {'name': survey.name, 'elements': []}
+        self.sections = [main_section]
+        build_sections(
+            main_section, self.survey, self.sections,
+            self.select_multiples, self.gps_fields, self.encoded_fields,
+            self.GROUP_DELIMITER)
+
+    def section_by_name(self, name):
+        matches = filter(lambda s: s['name'] == name, self.sections)
+        assert(len(matches) == 1)
+        return matches[0]
+
+    def get_valid_sheet_name(self, desired_name, existing_names):
+        # a sheet name has to be <= 31 characters and not a duplicate of an
+        # existing sheet
+        # truncate sheet_name to SHEET_NAME_MAX_CHARS
+        new_sheet_name = desired_name[:self.SHEET_NAME_MAX_CHARS]
+
+        # make sure its unique within the list
+        i = 1
+        generated_name = new_sheet_name
+        while generated_name in existing_names:
+            digit_length = len(str(i))
+            allowed_name_len = self.SHEET_NAME_MAX_CHARS - digit_length
+            # make name the required len
+            if len(generated_name) > allowed_name_len:
+                generated_name = generated_name[:allowed_name_len]
+            generated_name = "{0}{1}".format(generated_name, i)
+            i += 1
+        return generated_name
+         
+         
+    @classmethod
+    def format_field_title(cls, abbreviated_xpath, field_delimiter):
+        if field_delimiter != '/':
+            return field_delimiter.join(abbreviated_xpath.split('/'))
+        return abbreviated_xpath
+    
+    @classmethod
+    def split_select_multiples(cls, row, select_multiples):
+        # for each select_multiple, get the associated data and split it
+        for xpath, choices in select_multiples.iteritems():
+            # get the data matching this xpath
+            data = row.get(xpath)
+            selections = []
+            if data:
+                selections = [
+                    u'{0}/{1}'.format(
+                        xpath, selection) for selection in data.split()]
+            if not cls.BINARY_SELECT_MULTIPLES:
+                row.update(dict(
+                    [(choice, choice in selections if selections else None)
+                     for choice in choices]))
+            else:
+                YES = 1
+                NO = 0
+                row.update(dict(
+                    [(choice, YES if choice in selections else NO)
+                     for choice in choices]))
+        return row
+
+    @classmethod
+    def split_gps_components(cls, row, gps_fields):
+        # for each gps_field, get associated data and split it
+        for xpath, gps_components in gps_fields.iteritems():
+            data = row.get(xpath)
+            if data:
+                gps_parts = data.split()
+                if len(gps_parts) > 0:
+                    row.update(zip(gps_components, gps_parts))
+        return row
+
+    @classmethod
+    def decode_mongo_encoded_fields(cls, row, encoded_fields):
+        for xpath, encoded_xpath in encoded_fields.iteritems():
+            if row.get(encoded_xpath):
+                val = row.pop(encoded_xpath)
+                row.update({xpath: val})
+        return row
+
+    @classmethod
+    def decode_mongo_encoded_section_names(cls, data):
+        return dict([(_decode_from_mongo(k), v) for k, v in data.iteritems()])
+
+    @classmethod
+    def convert_type(cls, value, data_type):
+        """
+        Convert data to its native type e.g. string '1' to int 1
+        @param value: the string value to convert
+        @param data_type: the native data type to convert to
+        @return: the converted value
+        """
+        func = ExportBuilder.CONVERT_FUNCS.get(data_type, lambda x: x)
+        try:
+            return func(value)
+        except ValueError:
+            return value
+
+    def pre_process_row(self, row, section):
+        """
+        Split select multiples, gps and decode . and $
+        """
+        section_name = section['name']
+
+        # first decode fields so that subsequent lookups
+        # have decoded field names
+        if section_name in self.encoded_fields:
+            row = ExportBuilder.decode_mongo_encoded_fields(
+                row, self.encoded_fields[section_name])
+
+        if self.SPLIT_SELECT_MULTIPLES and\
+                section_name in self.select_multiples:
+            row = ExportBuilder.split_select_multiples(
+                row, self.select_multiples[section_name])
+
+        if section_name in self.gps_fields:
+            row = ExportBuilder.split_gps_components(
+                row, self.gps_fields[section_name])
+
+        # convert to native types
+        for elm in section['elements']:
+            # only convert if its in our list and its not empty, just to
+            # optimize
+            value = row.get(elm['xpath'])
+            if elm['type'] in ExportBuilder.TYPES_TO_CONVERT\
+                    and value is not None and value != '':
+                row[elm['xpath']] = ExportBuilder.convert_type(
+                    value, elm['type'])
+
+        return row
+    
+    @classmethod
+    def dict_to_joined_export(cls, data, index, indices, name):
+        """
+        Converts a dict into one or more tabular datasets
+        """
+        output = {}
+        # TODO: test for _geolocation and attachment lists
+        if isinstance(data, dict):
+            for key, val in data.iteritems():
+                if isinstance(val, list) and key not in [NOTES, TAGS]:
+                    output[key] = []
+                    for child in val:
+                        if key not in indices:
+                            indices[key] = 0
+                        indices[key] += 1
+                        child_index = indices[key]
+                        new_output = cls.dict_to_joined_export(
+                            child, child_index, indices, key)
+                        d = {INDEX: child_index, PARENT_INDEX: index,
+                             PARENT_TABLE_NAME: name}
+                        # iterate over keys within new_output and append to
+                        # main output
+                        for out_key, out_val in new_output.iteritems():
+                            if isinstance(out_val, list):
+                                if out_key not in output:
+                                    output[out_key] = []
+                                output[out_key].extend(out_val)
+                            else:
+                                d.update(out_val)
+                        output[key].append(d)
+                else:
+                    if name not in output:
+                        output[name] = {}
+                    if key in [TAGS]:
+                        output[name][key] = ",".join(val)
+                    elif key in [NOTES]:
+                        output[name][key] = "\r\n".join(
+                            [v['note'] for v in val])
+                    else:
+                        output[name][key] = val
+
+        return output
