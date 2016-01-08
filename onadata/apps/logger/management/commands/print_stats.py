d791d9e84ff4cdf157d3ddb787082b2fde48301d
John N. Milner <john@tmoj.net>
2016-01-08 12:28:21 -0500

    print_stats command tweaks

--- onadata/apps/logger/management/commands/print_stats.py
+++ onadata/apps/logger/management/commands/print_stats.py
@@ -9,6 +9,10 @@ from optparse import make_option
 from onadata.apps.logger.models import Instance
 from onadata.apps.logger.models import XForm
 
+INSTANCE_REPORT_FILE = '/tmp/instances.csv'
+XFORM_REPORT_FILE = '/tmp/xforms.csv'
+USER_REPORT_FILE = '/tmp/users.csv'
+
 def _date(dd):
     return dd.strftime('%D-%T')
 
@@ -24,28 +28,28 @@ def write_csv(filename, fieldnames, data):
             writer.writerow(d)
 
 def _print_instances():
-    fields = ('status', 'date_created', 'user_id', 'survey_type_id')
+    fields = ('status', 'date_created', 'pk', 'user_id', 'xform_id')
     instances = Instance.objects.all().values(*fields)
     data = [sanitize(ii) for ii in instances]
-    write_csv('/tmp/instances.csv', fields, data)
+    write_csv(INSTANCE_REPORT_FILE, fields, data)
 
 def _print_xforms():
-    fields = ('date_created', 'num_of_submissions', 'user_id')
+    fields = ('date_created', 'num_of_submissions', 'pk', 'user_id')
     xforms = XForm.objects.all().values(*fields)
     data = [sanitize(ii) for ii in xforms]
-    write_csv('/tmp/xforms.csv', fields, data)
+    write_csv(XFORM_REPORT_FILE, fields, data)
 
 def _print_users():
     fields = ('date_joined', 'last_login', 'pk')
     users = User.objects.all().values(*fields)
-    write_csv('/tmp/users.csv', fields, users)
+    write_csv(USER_REPORT_FILE, fields, users)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print 'xforms...'
+        print 'Writing xforms report to "{}"...'.format(XFORM_REPORT_FILE)
         _print_xforms()
-        print 'instances...'
+        print 'Writing instancess report to "{}"...'.format(INSTANCE_REPORT_FILE)
         _print_instances()
-        print 'users...'
+        print 'Writing users report to "{}"...'.format(USER_REPORT_FILE)
         _print_users()
 
