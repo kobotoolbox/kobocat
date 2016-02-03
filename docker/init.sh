84338fa4cfb56789a81196fa2aea6a798fac632c
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-02-03 11:02:12 -0500

    Move PostGIS initialization to Postgres container.

--- docker/init.sh
+++ docker/init.sh
@@ -3,7 +3,7 @@
 oldpwd=$(pwd)
 cd /srv/src/kobocat
 
-psql -h db -p ${PSQL_PORT} -U ${KOBO_PSQL_DB_USER} ${KOBO_PSQL_DB_NAME} -c "create extension postgis; create extension postgis_topology"
+[ -e /tmp/computed_vars.source.bash ] && source /tmp/computed_vars.source.bash
  
 python manage.py syncdb --noinput
 
