4facb5b9325e08e32195671a5d56b610c32f7177
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-02-11 17:15:38 -0500

    Wait for Postgres.

--- docker/init.sh
+++ docker/init.sh
@@ -4,11 +4,26 @@ set -e
 oldpwd=$(pwd)
 cd /srv/src/kobocat
 
-# FIXME: Convince South that KPI has already done the overlapping migrations.
-python manage.py migrate --fake oauth2_provider
+echo 'Waiting for Postgres.'
+KOBO_PSQL_DB_NAME=${KOBO_PSQL_DB_NAME:-"kobotoolbox"}
+KOBO_PSQL_DB_USER=${KOBO_PSQL_DB_USER:-"kobo"}
+KOBO_PSQL_DB_PASS=${KOBO_PSQL_DB_PASS:-"kobo"}
+dockerize -timeout=20s -wait ${PSQL_PORT}
+until $(PGPASSWORD="${KOBO_PSQL_DB_PASS}" psql -d ${KOBO_PSQL_DB_NAME} -h psql -U ${KOBO_PSQL_DB_USER} -c '' 2> /dev/null); do
+    sleep 1
+done
+echo 'Postgres ready.'
 
+echo 'Synchronizing database.'
 python manage.py syncdb --noinput
 
+# FIXME: Convince South that KPI has already done the overlapping migrations.
+echo 'Running fake migrations.'
+python manage.py migrate --noinput --fake oauth2_provider
+
+echo 'Running migrations.'
 python manage.py migrate --noinput
+
+echo '\`kobocat\` initialization completed.'
 
 cd $oldpwd
