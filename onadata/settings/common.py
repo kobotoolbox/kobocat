59633b3008cd7ca6780710bc9a67901ebdbaff4b
John N. Milner <john@tmoj.net>
2016-03-01 11:41:44 -0500

    Re-enable DRF JSONRenderer; fixes #139

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -266,6 +266,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',
         'rest_framework_jsonp.renderers.JSONPRenderer',
         'rest_framework_xml.renderers.XMLRenderer',
         'rest_framework_csv.renderers.CSVRenderer',
