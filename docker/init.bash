0163e1f7f97e12565fd4c65916fba7da1283d6ce
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-03-09 13:09:51 -0500

    Update `init.bash` for Django 1.8.

--- docker/init.bash
+++ docker/init.bash
@@ -11,11 +11,8 @@ cd /srv/src/kobocat
 echo 'Synchronizing database.'
 python manage.py syncdb --noinput
 
-# FIXME: Convince South that KPI has already done the overlapping migrations.
-echo 'Running fake migrations.'
-python manage.py migrate --noinput --fake oauth2_provider
-
 echo 'Running migrations.'
+python manage.py makemigrations
 python manage.py migrate --noinput
 
 
