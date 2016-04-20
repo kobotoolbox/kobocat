c6d9c457225b211be1c6b12af11cd3b003c3186c
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-04-20 10:52:37 -0400

    Use Celery soft timeout to give time for failed task cleanup.

--- docker/run_celery.bash
+++ docker/run_celery.bash
@@ -3,7 +3,11 @@ set -e
 
 source /etc/profile
 
-CELERYD_OPTIONS='--loglevel=DEBUG --time-limit=900 --maxtasksperchild=5'
+CELERYD_TASK_SOFT_TIME_LIMIT="${CELERYD_TASK_SOFT_TIME_LIMIT:-$((15*60))}"
+# Give tasks 1 minute for exception handling and cleanup before killing timed out Celery processes.
+CELERYD_TASK_TIME_LIMIT="${CELERYD_TASK_TIME_LIMIT:-$((${CELERYD_TASK_SOFT_TIME_LIMIT}+60))}"
+
+CELERYD_OPTIONS="--loglevel=DEBUG --soft-time-limit=${CELERYD_TASK_SOFT_TIME_LIMIT} --time-limit=${CELERYD_TASK_TIME_LIMIT} --maxtasksperchild=5"
 
 cd "${KOBOCAT_SRC_DIR}"
 
