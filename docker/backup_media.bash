eaad0b77b105cbfabddf0e2986eb3a2d3f40f880
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-11-06 16:47:47 -0500

    Respect changes to `KOBOCAT_MEDIA_URL` in Docker automatic backups.

--- docker/backup_media.bash
+++ docker/backup_media.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 source /etc/profile
 
+KOBOCAT_MEDIA_URL="${KOBOCAT_MEDIA_URL:-'media'}"
+
 timestamp="$(date +%Y.%m.%d.%H_%M)"
-(cd "${KOBOCAT_SRC_DIR}" && tar cf "${BACKUPS_DIR}/kobocat_media__${timestamp}.tar" media)
+(cd "${KOBOCAT_SRC_DIR}" && tar cf "${BACKUPS_DIR}/kobocat_media__${timestamp}.tar" "${KOBOCAT_MEDIA_URL}")
