5d521d50ffaf417a42ee233ff868dbc6bba2dce4
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-11-07 17:56:34 -0500

    Typo fixes.

--- docker/backup_media.bash
+++ docker/backup_media.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 source /etc/profile
 
-KOBOCAT_MEDIA_URL="${KOBOCAT_MEDIA_URL:-'media'}"
+KOBOCAT_MEDIA_URL="${KOBOCAT_MEDIA_URL:-media}"
 
 timestamp="$(date +%Y.%m.%d.%H_%M)"
 (cd "${KOBOCAT_SRC_DIR}" && tar cf "${BACKUPS_DIR}/kobocat_media__${timestamp}.tar" "${KOBOCAT_MEDIA_URL}")
--- docker/run_tests.bash
+++ docker/run_tests.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
+set -e
 source /etc/profile
 
 echo "print('Django shell loaded successfully.')" | python manage.py shell
-
