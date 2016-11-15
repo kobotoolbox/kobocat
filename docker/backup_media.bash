1aeb56ff905ff92a3628ea1ec855bf45660ba767
Esmail Fadae <esmail.fadae@kobotoolbox.org>
2016-11-15 16:42:38 -0500

    Report successful backups.

--- docker/backup_media.bash
+++ docker/backup_media.bash
@@ -5,4 +5,8 @@ source /etc/profile
 KOBOCAT_MEDIA_URL="${KOBOCAT_MEDIA_URL:-media}"
 
 timestamp="$(date +%Y.%m.%d.%H_%M)"
-(cd "${KOBOCAT_SRC_DIR}" && tar cf "${BACKUPS_DIR}/kobocat_media__${timestamp}.tar" "${KOBOCAT_MEDIA_URL}")
+backup_filename="kobocat_media__${timestamp}.tar"
+
+(cd "${KOBOCAT_SRC_DIR}" && tar cf "${BACKUPS_DIR}/${backup_filename}" "${KOBOCAT_MEDIA_URL}")
+
+echo "Backup file \`${backup_filename}\` created successfully."
