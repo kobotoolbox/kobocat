b194c725009e61654f489fb0fa6fb1933cf05294
Olivier Léger <olivierleger@gmail.com>
2017-11-10 16:53:00 -0500

    Added SQL migrations

---
+++ onadata/apps/logger/migrations/0004_increase-length-of-attachment-mimetype-field.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('logger', '0003_add-index-on-attachment-media-file'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='attachment',
+            name='mimetype',
+            field=models.CharField(default=b'', max_length=100, blank=True),
+        ),
+    ]
