4b622475fd0f8b3d30b08a82f061f43dc404f063
Olivier Léger <olivierleger@gmail.com>
2017-12-15 16:37:28 -0500

    Added SQL migration for new permission 'validate_xform'

---
+++ onadata/apps/logger/migrations/0007_add_validate_permission_on_xform.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('logger', '0006_add_validation_status_json_field_in_instance_table'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='xform',
+            options={'ordering': ('id_string',), 'verbose_name': 'XForm', 'verbose_name_plural': 'XForms', 'permissions': (('view_xform', 'Can view associated data'), ('report_xform', 'Can make submissions to the form'), ('move_xform', 'Can move form between projects'), ('transfer_xform', 'Can transfer form ownership.'), ('validate_xform', 'Can validate submissions.'))},
+        ),
+    ]
