f0beba5d8e4cc330b3c2e8bcfced241df83d598f
John N. Milner <john@tmoj.net>
2018-02-07 11:42:09 -0500

    Skip `chown` of media files on container start

--- docker/sync_static.sh
+++ docker/sync_static.sh
@@ -11,9 +11,19 @@ mkdir -p "${KOBOCAT_SRC_DIR}/onadata/static"
 echo "Collecting static files..."
 python manage.py collectstatic -v 0 --noinput
 echo "Done"
-echo "Fixing permissions..."
-chown -R "${UWSGI_USER}" "${KOBOCAT_SRC_DIR}"
-echo "Done."
+
+# `chown` becomes very slow once a fair amount of media has been collected.
+#echo "Fixing permissions..."
+#chown -R "${UWSGI_USER}" "${KOBOCAT_SRC_DIR}"
+#echo "Done."
+echo '%%%%%%% NOTICE %%%%%%%'
+echo '% To avoid long delays, we no longer reset ownership of media files'
+echo '% every time this container starts. If you have trouble with'
+echo '% permissions, please run the following command inside the'
+echo '% `kobocat` container:'
+echo "%	chown -R \"${UWSGI_USER}\" \"${KOBOCAT_SRC_DIR}\""
+echo '%%%%%%%%%%%%%%%%%%%%%%'
+
 echo "Syncing to nginx folder..."
 rsync -aq ${KOBOCAT_SRC_DIR}/onadata/static/* /srv/static/
 echo "Done"
