075d1abe60318a7b466ad6542bd779f23a76de39
John N. Milner <john@tmoj.net>
2018-05-29 15:31:01 -0400

    Reset media directory ownership (*not* recursively)

--- docker/sync_static.sh
+++ docker/sync_static.sh
@@ -12,12 +12,13 @@ echo "Collecting static files..."
 python manage.py collectstatic -v 0 --noinput
 echo "Done"
 
-# `chown` becomes very slow once a fair amount of media has been collected.
-#echo "Fixing permissions..."
-#chown -R "${UWSGI_USER}" "${KOBOCAT_SRC_DIR}"
-#echo "Done."
+# `chown -R` becomes very slow once a fair amount of media has been collected,
+# so reset ownership of the media directory *only*. See #379, #449
+echo "Resetting ownership of media directory..."
+chown "${UWSGI_USER}" "${KOBOCAT_SRC_DIR}/media"
+echo "Done."
 echo '%%%%%%% NOTICE %%%%%%%'
-echo '% To avoid long delays, we no longer reset ownership of media files'
+echo '% To avoid long delays, we no longer reset ownership *recursively*'
 echo '% every time this container starts. If you have trouble with'
 echo '% permissions, please run the following command inside the'
 echo '% `kobocat` container:'
