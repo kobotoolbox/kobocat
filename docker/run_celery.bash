b83a231ccaacaa2f99abb320d8bfe8cb97b158fd
Olivier Léger <olivierleger@gmail.com>
2019-01-25 15:15:11 -0500

    Start celery worker with a prefixed hostname to avoid conflit with kpi. Restored Ofair option

--- docker/run_celery.bash
+++ docker/run_celery.bash
@@ -4,7 +4,7 @@ source /etc/profile
 
 # Run the main Celery worker
 cd "${KOBOCAT_SRC_DIR}"
-exec celery worker -A onadata --loglevel=debug \
-    --hostname=main_worker@%h \
+exec celery worker -A onadata -Ofair --loglevel=info \
+    --hostname=kobocat_main_worker@%h \
     --logfile=${KOBOCAT_LOGS_DIR}/celery.log \
     --pidfile=/tmp/celery.pid
