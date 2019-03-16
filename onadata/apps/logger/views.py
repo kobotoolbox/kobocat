64524f563adf093a40d13266a1fd60737c9e1f07
Olivier Leger <olivierleger@gmail.com>
2019-03-16 15:17:11 -0400

    Used 'request.get_host()' instead of 'request.META.get('HTTP_HOST')

--- onadata/apps/logger/views.py
+++ onadata/apps/logger/views.py
@@ -735,7 +735,7 @@ def ziggy_submissions(request, username):
 @user_passes_test(lambda u: u.is_superuser)
 def superuser_stats(request, username):
     base_filename = '{}_{}_{}.zip'.format(
-        re.sub('[^a-zA-Z0-9]', '-', request.META['HTTP_HOST']),
+        re.sub('[^a-zA-Z0-9]', '-', request.get_host()),
         datetime_module.date.today(),
         datetime_module.datetime.now().microsecond
     )
