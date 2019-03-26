af6b20108c8ca33437b78f71dca522f8536925d8
Olivier Leger <olivierleger@gmail.com>
2019-03-26 11:31:12 -0400

    Refactored script to fetch data by chunks

--- onadata/apps/viewer/management/commands/rewrite_attachments_urls_in_mongo.py
+++ onadata/apps/viewer/management/commands/rewrite_attachments_urls_in_mongo.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import json
 import sys
+import time
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -10,38 +11,80 @@ from django.utils.http import urlencode
 from django.utils.translation import ugettext_lazy as _
 
 from onadata.apps.viewer.models.parsed_instance import xform_instances
+from onadata.apps.logger.models.instance import Instance
 
 
 class Command(BaseCommand):
 
     help = _("Rewrite attachments urls to point to new protected endpoint")
+    BATCH_SIZE = 1000
 
     def handle(self, *args, **kwargs):
 
+        self.__cached_instances = {}
+        self.__last_id = None
+
+        cursor = self.__get_data()
+        instances_count = cursor.count()
+        done = 0
+        stop = False
+        while stop is not True:
+            batch_count = cursor.count(True)
+            if batch_count > 0:
+                for instance in cursor:
+                    for attachment in instance.get("_attachments"):
+                        try:
+                            if type(attachment) != dict:  # seems that old instances, do not use the
+                                filename = attachment
+                                xform_id, id, mimetype = self.__get_relationship(instance.get("_id"), filename)
+                                attachment = {
+                                    "mimetype": mimetype,
+                                    "download_url": self.__secure_url(filename),
+                                    "filename": filename,
+                                    "instance": instance.get("_id"),
+                                    "id": id,
+                                    "xform": xform_id
+                                }
+                            else:
+                                filename = attachment.get("filename")
+                                attachment["download_url"] = self.__secure_url(filename)
+
+                            for suffix in settings.THUMB_CONF.keys():
+                                attachment["download_{}_url".format(suffix)] = self.__secure_url(filename, suffix)
+
+                        except Exception as e:
+                            print("ERROR - {}".format(str(e)))
+                            print(instance)
+
+                    done += 1
+                    self.__last_id = instance.get("_id")
+                    xform_instances.save(instance)
+                    progress = "\r(%s/%s records) - %.2f %% done..." % (done, instances_count, (float(done) / float(instances_count)) * 100)
+                    sys.stdout.write(progress)
+                    sys.stdout.flush()
+
+                settings.MONGO_CONNECTION.admin.command({'fsync': 1})
+                cursor = self.__get_data()
+            else:
+                stop = True
+
+        sys.stdout.write("\nUpdated %s records\n" % instances_count)
+
+    def __get_data(self):
         query = {"$and": [
             {"_deleted_at": {"$exists": False}},
             {"_deleted_at": None},
             {"_attachments": {"$ne": ""}},
-            {"_attachments": {"$ne": []}}
+            {"_attachments": {"$ne": []}},
+            {"_attachments.download_small_url": {"$exists": False}}
         ]}
 
-        results = xform_instances.find(query)
-        instances_count = results.count()
-        done = 0
-        if instances_count > 0:
-            for instance in results:
-                for attachment in instance.get("_attachments"):
-                    filename = attachment.get("filename")
-                    attachment["download_url"] = self.__secure_url(filename)
-                    for suffix in settings.THUMB_CONF.keys():
-                        attachment["download_{}_url".format(suffix)] = self.__secure_url(filename, suffix)
+        if self.__last_id is not None:
+            query.update({
+                "_id": {"$gt": self.__last_id}
+            })
 
-                done += 1
-                time.sleep(1)
-
-                xform_instances.save(instance)
-                sys.stdout.write(
-                    "%.2f %% done ...\r" % ((float(done) / float(instances_count)) * 100))
+        return xform_instances.find(query).limit(self.BATCH_SIZE)
 
     @staticmethod
     def __secure_url(filename, suffix="original"):
@@ -57,3 +100,35 @@ class Command(BaseCommand):
             suffix=suffix,
             media_file=urlencode({"media_file": filename})
         )
+
+    def __get_relationship(self, instance_id, filename):
+        """
+        Retrieves XForm, Attachment Id & mimetype of filename
+
+        This method can be hard on RAM.
+
+        Maybe needs some garbage collecting logic
+        :param instance_id: int
+        :param filename: str
+        :return: tuple
+        """
+
+        if instance_id not in self.__cached_instances:
+            instance = Instance.objects.get(id=instance_id)
+            self.__cached_instances[instance_id] = {
+                "xform_id": instance.xform.id,
+                "attachments": [{
+                    "mimetype": attachment.mimetype,
+                    "filename": attachment.media_file,
+                    "id": attachment.id
+                } for attachment in instance.attachments.all()]
+            }
+
+        for attachment in self.__cached_instances[instance_id].get("attachments"):
+            if filename == attachment.get("filename"):
+                return (self.__cached_instances[instance_id].get("xform_id"),
+                        attachment.get("id"),
+                        attachment.get("mimetype"))
+
+        return None, None, None
+
