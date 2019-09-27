6c13a34c5963141156c37ea9048304036e7af84a
Olivier Léger <olivierleger@gmail.com>
2019-09-27 10:58:08 -0400

    Removed deprecated RestServices

--- onadata/apps/restservice/tests/test_restservice.py
+++ onadata/apps/restservice/tests/test_restservice.py
@@ -1,12 +1,6 @@
-import os
-import time
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-from django.core.urlresolvers import reverse
-from nose import SkipTest
-from pybamboo.connection import Connection
-from pybamboo.dataset import Dataset
-
-from onadata.apps.main.views import show, link_to_bamboo
 from onadata.apps.main.tests.test_base import TestBase
 from onadata.apps.logger.models.xform import XForm
 from onadata.apps.restservice.RestServiceInterface import RestServiceInterface
@@ -15,22 +9,10 @@ from onadata.apps.restservice.models import RestService
 
 class RestServiceTest(TestBase):
 
-    def setUp(self):
-        self.service_url = u'http://0.0.0.0:8001/%(id_string)s/post/%(uuid)s'
-        self.service_name = u'f2dhis2'
-        self._create_user_and_login()
-        filename = u'dhisform.xls'
-        self.this_directory = os.path.dirname(__file__)
-        path = os.path.join(self.this_directory, u'fixtures', filename)
-        self._publish_xls_file(path)
-        self.xform = XForm.objects.all().reverse()[0]
-
-    def wait(self, t=1):
-        time.sleep(t)
-
     def _create_rest_service(self):
-        rs = RestService(service_url=self.service_url,
-                         xform=self.xform, name=self.service_name)
+        rs = RestService(service_url='/api/v2/assets/aAAAAAAAAAAA/hook-signal/',
+                         xform=XForm.objects.all().reverse()[0],
+                         name='kpi_hook')
         rs.save()
         self.restservice = rs
 
