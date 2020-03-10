6c250dd2bc384e463b13105379e6e689453af84f
Olivier Leger <olivierleger@gmail.com>
2020-03-10 17:21:55 -0300

    Block users to list all other users

--- onadata/apps/api/viewsets/user_viewset.py
+++ onadata/apps/api/viewsets/user_viewset.py
@@ -1,10 +1,11 @@
+# coding: utf-8
 from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from onadata.apps.api.permissions import UserProfilePermissions
 from onadata.libs.serializers.user_serializer import UserSerializer
-from onadata.apps.api import permissions
 
 
 class UserViewSet(ReadOnlyModelViewSet):
@@ -52,10 +53,10 @@ This endpoint allows you to list and retrieve user's first and last names.
 """
     queryset = User.objects.exclude(pk=settings.ANONYMOUS_USER_ID)
     serializer_class = UserSerializer
-    permission_classes = [permissions.DjangoObjectPermissionsAllowAnon]
+    permission_classes = [UserProfilePermissions]
 
     # This is NOT DRF lookup_field. DRF lookup_field is only located on
-    # seralizers and is deprecated and replaced by Meta.extra_kwargs['url']['lookup field']
+    # serializers and is deprecated and replaced by Meta.extra_kwargs['url']['lookup field']
     # This field is has been added by the previous dev, and is used to generate
     # a custom url routing on the fly. Basically it will be added in the
     # url regex.
