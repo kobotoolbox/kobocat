c0d437965278259e0f97de69939354e70f3ce6fc
Olivier Leger <olivierleger@gmail.com>
2020-06-09 16:37:19 -0300

    Increased request body & file upload max size

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -584,3 +584,11 @@ if os.environ.get('PUBLIC_REQUEST_SCHEME', '').lower() == 'https':
 PASSWORD_HASHERS = (
     'onadata.libs.utils.hashers.PBKDF2PasswordHasher150KIterations',
 ) + PASSWORD_HASHERS
+
+
+# The maximum size in bytes that a request body may be before a SuspiciousOperation (RequestDataTooBig) is raised
+# This variable is available only in Django 1.10+. Only there for next upgrade
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
+# The maximum size (in bytes) that an upload will be before it gets streamed to the file system
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
