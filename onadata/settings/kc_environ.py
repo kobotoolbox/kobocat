3aed44c2b4598ea9fedd6bf47b83e421b2367cf9
Olivier Leger <olivierleger@gmail.com>
2020-07-20 12:20:59 -0400

    Comment out deprecated "TEMPLATE_CONTEXT_PROCESSORS"

--- onadata/settings/kc_environ.py
+++ onadata/settings/kc_environ.py
@@ -106,9 +106,9 @@ KOBOFORM_URL = os.environ.get("KOBOFORM_URL", "http://kf.kobo.local")
 KOBOCAT_URL = os.environ.get("KOBOCAT_URL", "http://kc.kobo.local")
 
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'onadata.koboform.context_processors.koboform_integration',
-) + TEMPLATE_CONTEXT_PROCESSORS
+#TEMPLATE_CONTEXT_PROCESSORS = (
+#    'onadata.koboform.context_processors.koboform_integration',
+#) + TEMPLATE_CONTEXT_PROCESSORS
 
 MIDDLEWARE.insert(0, 'onadata.koboform.redirect_middleware.ConditionalRedirects')
 
