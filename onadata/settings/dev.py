7dc27fd1329c4742678dd5a75d6ecd6399d3b140
Olivier Leger <olivierleger@gmail.com>
2020-07-23 15:02:25 -0400

    Improved debug logging in dev environment

--- onadata/settings/dev.py
+++ onadata/settings/dev.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 from .prod import *
 
-
 LOGGING['handlers']['console'] = {
     'level': 'DEBUG',
     'class': 'logging.StreamHandler',
-    'formatter': 'verbose'
+    'formatter': 'verbose',
+    'stream': sys.stdout,
 }
-
-#sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+LOGGING['root'] = {
+    'handlers': ['console'],
+    'level': 'DEBUG'
+}
