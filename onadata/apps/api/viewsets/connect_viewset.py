0786d96da20b628f5a355c625ff3540c311d854b
Olivier Leger <olivierleger@gmail.com>
2020-07-28 15:13:11 -0400

    Removed extra __future__ imports after merge

--- onadata/apps/api/viewsets/connect_viewset.py
+++ onadata/apps/api/viewsets/connect_viewset.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-from __future__ import unicode_literals, print_function, division, absolute_import
-
 from rest_framework import viewsets
 from rest_framework.response import Response
 
--- onadata/libs/serializers/user_profile_serializer.py
+++ onadata/libs/serializers/user_profile_serializer.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-from __future__ import unicode_literals, print_function, division, absolute_import
-
 from rest_framework import serializers
 
 from onadata.apps.main.models import UserProfile
