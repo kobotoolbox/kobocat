483fb798b21c5f3a0646118215c4fad6cb908d5a
Michał Łazowik <m1@qed.ai>
2020-10-12 23:05:17 +0200

    Update user email when using remote auth

--- onadata/auth.py
+++ onadata/auth.py
@@ -28,3 +28,24 @@ class QedRemoteUserAuth(BaseAuthentication):
         user = authenticate(remote_user=request.META.get(self.header))
         if user and user.is_active:
             return (user, None)
+
+
+class QedRemoteUserAttributeMiddleware(object):
+    username_header = 'HTTP_X_AUTH_USERNAME'
+    email_header = 'HTTP_X_AUTH_EMAIL'
+
+    def process_request(self, request):
+        username = request.META.get(self.username_header, None)
+        email = request.META.get(self.email_header, None)
+
+        if hasattr(request, 'user') and request.user.is_authenticated and \
+                username and email:
+            user = request.user
+            needs_saving = False
+
+            if user.email != email:
+                user.email = email
+                needs_saving = True
+
+            if needs_saving:
+                user.save()
