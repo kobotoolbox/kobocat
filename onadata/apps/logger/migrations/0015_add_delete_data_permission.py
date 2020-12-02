3df76742ad9d9a06953511ba02071595b3f483af
Olivier Leger <olivierleger@gmail.com>
2020-12-02 15:19:14 -0500

    Use "bulk_create" to add new permission in migration

--- onadata/apps/logger/migrations/0015_add_delete_data_permission.py
+++ onadata/apps/logger/migrations/0015_add_delete_data_permission.py
@@ -24,13 +24,24 @@ def forwards_func(apps, schema_editor):
     create_permissions(apps, verbosity=0)
     apps.models_module = None
 
-    permission = Permission.objects.get(codename='delete_data_xform')
-    users = User.objects.exclude(pk=AnonymousUser().pk)
-    for user_ in users.all():
-        user_.user_permissions.add(permission)
+    permission = Permission.objects.get(content_type__app_label='logger',
+                                        codename='delete_data_xform')
+    user_ids = (
+        User.objects.values_list('pk', flat=True).exclude(pk=AnonymousUser().pk)
+    )
+    ThroughModel = User.user_permissions.through  # noqa
+
+    through_models = []
+    for user_id in user_ids:
+        through_models.append(ThroughModel(user_id=user_id,
+                                           permission_id=permission.pk))
+    ThroughModel.objects.bulk_create(through_models)
 
 
 def reverse_func(apps, schema_editor):
+    """
+    Revert 'delete_data_xform' permission. It can take a while on big databases
+    """
     users = User.objects.exclude(pk=AnonymousUser().pk)
     permission = Permission.objects.get(codename='delete_data_xform')
     for user_ in users.all():
@@ -48,6 +59,5 @@ class Migration(migrations.Migration):
             name='xform',
             options={'ordering': ('id_string',), 'verbose_name': 'XForm', 'verbose_name_plural': 'XForms', 'permissions': (('view_xform', 'Can view associated data'), ('report_xform', 'Can make submissions to the form'), ('move_xform', 'Can move form between projects'), ('transfer_xform', 'Can transfer form ownership'), ('validate_xform', 'Can validate submissions'), ('delete_data_xform', 'Can delete submissions'))},
         ),
-        # This can take a while on big databases
         migrations.RunPython(forwards_func, reverse_func),
     ]
