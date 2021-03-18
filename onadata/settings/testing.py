2b82d556786faa282c1d2341879cb998314d4586
Olivier Léger <olivierleger@gmail.com>
2021-03-18 17:33:58 -0400

    Adapt GH action YML file for Python3.8. Add beta to trigger actions and use conditional testing settings (PostgreSQL or SQLite)

--- onadata/settings/testing.py
+++ onadata/settings/testing.py
@@ -10,17 +10,24 @@ from .base import *
 # Django Framework settings    #
 ################################
 
+# By default, PostgreSQL is considered as the default engine. Some DB queries
+# rely on PostgreSQL engine to be executed. It needs to be set to `False` to
+# make them run with SQLite (i.e. running unit tests locally)
 # Database (i.e. PostgreSQL)
 DATABASES = {
     'default': dj_database_url.config(
         env='TEST_DATABASE_URL', default="sqlite:///%s/db.sqlite3" % BASE_DIR)
 }
 
-# Need to add these lines to make the tests run
-# Moreover, `apt-get update && apt-get install libsqlite3-mod-spatialite`
-#  should be executed inside the container
-DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.spatialite"
-SPATIALITE_LIBRARY_PATH = os.environ.get('SPATIALITE_LIBRARY_PATH', 'mod_spatialite')
+if not os.getenv('USE_POSTGRESQL', False):
+    # Need to add these lines to make the tests run
+    # Moreover, `apt-get update && apt-get install libsqlite3-mod-spatialite`
+    #  should be executed inside the container
+    DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.spatialite"
+    SPATIALITE_LIBRARY_PATH = os.environ.get('SPATIALITE_LIBRARY_PATH', 'mod_spatialite')
+else:
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+print('DATABASES', DATABASES, flush=True)
 
 MEDIA_ROOT = '/tmp/test_media/'
 
@@ -44,6 +51,7 @@ SECRET_KEY = os.urandom(50).hex()
 TESTING_MODE = True
 TEST_HTTP_HOST = 'testserver.com'
 TEST_USERNAME = 'bob'
+USE_POSTGRESQL = os.getenv('USE_POSTGRESQL', False)
 
 ################################
 # Celery settings              #
