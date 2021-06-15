32294a661b2a44455ebe16e976cba5a8aaf39ae8
John N. Milner <john@tmoj.net>
2021-06-14 23:28:33 -0400

    Fix bad merge; original commit was 58f147228

--- onadata/settings/base.py
+++ onadata/settings/base.py
@@ -498,8 +498,6 @@ AWS_S3_HOST = os.environ.get("KOBOCAT_AWS_S3_HOST", "s3.amazonaws.com")
 GOOGLE_ANALYTICS_PROPERTY_ID = os.environ.get("GOOGLE_ANALYTICS_TOKEN", False)
 GOOGLE_ANALYTICS_DOMAIN = "auto"
 
-MONGO_DB_MAX_TIME_MS = CELERY_TASK_TIME_LIMIT * 1000
-
 # duration to keep zip exports before deletion (in seconds)
 ZIP_EXPORT_COUNTDOWN = 24 * 60 * 60
 
@@ -673,7 +671,8 @@ MONGO_CONNECTION = MongoClient(
 MONGO_DB = MONGO_CONNECTION[MONGO_DATABASE['NAME']]
 
 # Timeout for Mongo, must be, at least, as long as Celery timeout.
-MONGO_DB_MAX_TIME_MS = CELERY_TASK_TIME_LIMIT * 60 * 1000
+MONGO_DB_MAX_TIME_MS = CELERY_TASK_TIME_LIMIT * 1000
+
 
 ################################
 # Sentry settings              #
