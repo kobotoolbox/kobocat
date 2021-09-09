kobotoolbox/kobocat

commit a2dbb91ce6742e0d33b0a2538f7a8b28d855a0fb

Disable seeking when writing ZIP exports
This is a hotfix for #475

onadata/libs/utils/viewer_tools.py
@@ -227,6 +227,17 @@ def enketo_url(form_url, id_string, instance_xml=None,
 def create_attachments_zipfile(attachments, output_file=None):
     if not output_file:
         output_file = NamedTemporaryFile()
+    else:
+        # Disable seeking in a way understood by Python's zipfile module. See
+        # https://github.com/python/cpython/blob/ca2009d72a52a98bf43aafa9ad270a4fcfabfc89/Lib/zipfile.py#L1270-L1274
+        # This is a workaround for https://github.com/kobotoolbox/kobocat/issues/475
+        # and https://github.com/jschneier/django-storages/issues/566
+        def no_seeking(*a, **kw):
+            raise AttributeError(
+                'Seeking disabled! See '
+                'https://github.com/kobotoolbox/kobocat/issues/475'
+            )
+        output_file.seek = no_seeking
 
     storage = get_storage_class()()
     with zipfile.ZipFile(output_file, 'w', zipfile.ZIP_STORED, allowZip64=True) as zip_file:

commit a81fc68b24fa14ae28fe8fefe782598edb5ecd7b

Fix local ZIP exports
(by _not_ crashing when `FileSystemStorage` does not allow overwriting the
`seek` method on an export file)

Fixes an issue caused by a2dbb91ce6742e0d33b0a2538f7a8b28d855a0fb

See discussion at
https://community.kobotoolbox.org/t/unable-to-download-zipped-media-files/21142/9?u=jnm

onadata/libs/utils/logger_tools.py
@@ -436,6 +436,7 @@ def safe_create_instance(username, xml_file, media_files, uuid, request):
 
 
 def report_exception(subject, info, exc_info=None):
+    # TODO: replace with standard logging (i.e. `import logging`)
     if exc_info:
         cls, err = exc_info[:2]
         message = _("Exception in request:"

onadata/libs/utils/viewer_tools.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import os
 import json
+import logging
 import traceback
 import requests
 import zipfile
@@ -9,7 +10,7 @@ from tempfile import NamedTemporaryFile
 from xml.dom import minidom
 
 from django.conf import settings
-from django.core.files.storage import get_storage_class
+from django.core.files.storage import get_storage_class, FileSystemStorage
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.mail import mail_admins
 from django.utils.translation import ugettext as _
@@ -254,7 +255,18 @@ def create_attachments_zipfile(attachments, output_file=None):
                 'Seeking disabled! See '
                 'https://github.com/kobotoolbox/kobocat/issues/475'
             )
-        output_file.seek = no_seeking
+        try:
+            output_file.seek = no_seeking
+        except AttributeError as e:
+            # The default, file-system storage won't allow changing the `seek`
+            # attribute, which is fine because seeking on local files works
+            # perfectly anyway
+            storage_class = get_storage_class()
+            if not issubclass(storage_class, FileSystemStorage):
+                logging.warning(
+                    f'{storage_class} may not be a local storage class, but '
+                    f'disabling seeking failed: {e}'
+                )
 
     storage = get_storage_class()()
     with zipfile.ZipFile(output_file, 'w', zipfile.ZIP_STORED, allowZip64=True) as zip_file: