15cea3d683aa8f6e350ca717baa6291eab5c9311
Olivier Leger <olivierleger@gmail.com>
2022-02-18 18:22:35 -0500

    Remove full DEBUG output in console in dev mode

--- onadata/settings/dev.py
+++ onadata/settings/dev.py
@@ -6,11 +6,6 @@ from .base import *
 # Django Framework settings    #
 ################################
 
-LOGGING['root'] = {
-    'handlers': ['console'],
-    'level': 'DEBUG'
-}
-
 SESSION_ENGINE = "redis_sessions.session"
 SESSION_REDIS = RedisHelper.config(default="redis://redis_cache:6380/2")
 
