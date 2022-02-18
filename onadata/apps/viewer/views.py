6014a01df90d95d5458bc3f143b89bd41993e1a9
Olivier Leger <olivierleger@gmail.com>
2022-02-18 18:23:22 -0500

    Block access to replaced attachments

--- onadata/apps/viewer/views.py
+++ onadata/apps/viewer/views.py
@@ -397,6 +397,10 @@ def attachment_url(request, size='medium'):
             media_file_logger.info('attachment not found')
             return HttpResponseNotFound(_('Attachment not found'))
 
+        # Attachment has a replaced date, it should not be shown anymore
+        if attachment.replaced_at:
+            return HttpResponseNotFound(_('Attachment not found'))
+
         # Checks whether users are allowed to see the media file before giving them
         # the url
         xform = attachment.instance.xform
