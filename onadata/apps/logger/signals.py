9979941838c65dbec3da76db641c8c85de86c6ba
John N. Milner <john@tmoj.net>
2022-02-22 12:11:13 -0500

    Supplicate before the gods of Django and…
    do not dare to change the name of the `instance` parameter of signal
    handlers

--- onadata/apps/logger/signals.py
+++ onadata/apps/logger/signals.py
@@ -7,12 +7,16 @@ from onadata.apps.logger.models.attachment import Attachment
 
 
 @receiver(pre_delete, sender=Attachment)
-def pre_delete_attachment(attachment, **kwargs):
+def pre_delete_attachment(instance, **kwargs):
     # "Model.delete() isn’t called on related models, but the pre_delete and
     # post_delete signals are sent for all deleted objects." See
     # https://docs.djangoproject.com/en/2.2/ref/models/fields/#django.db.models.CASCADE
     # We want to delete all files when an Instance (or Attachment) object is
     # deleted.
+
+    # `instance` here means "model instance", and no, it is not allowed to
+    # change the name of the parameter
+    attachment = instance
     try:
         attachment.media_file.delete()
     except Exception as e:
