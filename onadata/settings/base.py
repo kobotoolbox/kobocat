3ae5a0a72604576cdb6a5e126cf6da56f937214b
David Burke <david@burkesoftware.com>
2022-03-02 13:40:08 -0500

    Remove typo

--- onadata/settings/base.py
+++ onadata/settings/base.py
@@ -26,7 +26,7 @@ def skip_suspicious_operations(record):
     if record.exc_info:
         exc_value = record.exc_info[1]
         if isinstance(exc_value, SuspiciousOperation):
-            return FalseSE
+            return False
     return True
 
 
