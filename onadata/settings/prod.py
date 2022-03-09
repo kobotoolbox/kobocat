bc5e110184bfe7838e52d653c39ad2b6b2a4d8cb
David Burke <david@burkesoftware.com>
2022-03-09 10:32:22 -0500

    '!

--- onadata/settings/prod.py
+++ onadata/settings/prod.py
@@ -9,7 +9,7 @@ from .base import *
 DEBUG = False
 TEMPLATES[0]['OPTIONS']['debug'] = False
 
-SESSION_ENGINE = "redis_sessions.session"
+SESSION_ENGINE = 'redis_sessions.session'
 # django-redis-session expects a dictionary with `url`
-redis_session_url = env.cache_url("REDIS_SESSION_URL", default="redis://redis_cache:6380/2")
-SESSION_REDIS = {"url": redis_session_url['LOCATION']}
+redis_session_url = env.cache_url('REDIS_SESSION_URL', default='redis://redis_cache:6380/2')
+SESSION_REDIS = {'url': redis_session_url['LOCATION']}
