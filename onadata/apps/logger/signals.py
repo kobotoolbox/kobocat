d524dfd1cf16c5ed82a903f1b4814aa725a65502
JacquelineMorrissette <jackie@kobotoolbox.org>
2022-05-19 15:18:23 -0400

    Created signals to add and remove attachment sizes to attachment_storage_bytes counts

--- onadata/apps/logger/signals.py
+++ onadata/apps/logger/signals.py
@@ -1,9 +1,44 @@
 # coding: utf-8
 import logging
-from django.db.models.signals import pre_delete
+
+from django.db.models import F
+from django.db.models.signals import (
+    pre_delete,
+    post_save,
+)
 from django.dispatch import receiver
 
 from onadata.apps.logger.models.attachment import Attachment
+from onadata.apps.logger.models.xform import XForm
+from onadata.apps.main.models.user_profile import UserProfile
+
+
+@receiver(pre_delete, sender=Attachment)
+def delete_user_profile_attachment_storage_bytes(instance, **kwargs):
+    """
+    Update the attachment_storage_bytes field in the UserProfile model
+    when an attachment is deleted
+    """
+    file_size = instance.media_file.size
+    owner_profile = instance.instance.xform.user.profile.pk
+    queryset = UserProfile.objects.filter(pk=owner_profile)
+    queryset.update(
+        attachment_storage_bytes=F('attachment_storage_bytes') - file_size
+    )
+
+
+@receiver(pre_delete, sender=Attachment)
+def delete_xform_attachment_storage_bytes(instance, **kwargs):
+    """
+    Update the attachment_storage_bytes field in the XForm
+    model when an attachment is deleted
+    """
+    file_size = instance.media_file.size
+    xform = instance.instance.xform.pk
+    queryset = XForm.objects.filter(pk=xform)
+    queryset.update(
+        attachment_storage_bytes=F('attachment_storage_bytes') - file_size
+    )
 
 
 @receiver(pre_delete, sender=Attachment)
@@ -21,3 +56,41 @@ def pre_delete_attachment(instance, **kwargs):
         attachment.media_file.delete()
     except Exception as e:
         logging.error('Failed to delete attachment: ' + str(e), exc_info=True)
+
+
+@receiver(post_save, sender=Attachment)
+def update_user_profile_attachment_storage_bytes(instance, created, **kwargs):
+    """
+    Update the attachment_storage_bytes field in the UserProfile model
+    when an attachment is added
+    """
+    if not created:
+        return
+    # Probably not necessary right now, but in case needed in the future
+    if getattr(instance, 'defer_counting', False):
+        return
+    file_size = instance.media_file.size
+    user_profile = instance.instance.xform.user.profile.pk
+    queryset = UserProfile.objects.filter(pk=user_profile)
+    queryset.update(
+        attachment_storage_bytes=F('attachment_storage_bytes') + file_size
+    )
+
+
+@receiver(post_save, sender=Attachment)
+def update_xform_attachment_storage_bytes(instance, created, **kwargs):
+    """
+    Update the attachment_storage_bytes field in the XForm model when
+    an attachment is added
+    """
+    if not created:
+        return
+    # Probably not needed now, but in case needed in the future
+    if getattr(instance, 'defer_counting', False):
+        return
+    file_size = instance.media_file.size
+    xform = instance.instance.xform.pk
+    queryset = XForm.objects.filter(pk=xform)
+    queryset.update(
+        attachment_storage_bytes=F('attachment_storage_bytes') + file_size
+    )
