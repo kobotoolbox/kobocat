1812615a477191cf7a91039f4f143de20a419f2b
David Burke <david@burkesoftware.com>
2022-06-28 11:12:03 -0400

    Address digest spelling and style issues

--- onadata/apps/django_digest_backends/tests.py
+++ onadata/apps/django_digest_backends/tests.py
@@ -5,36 +5,36 @@ from .cache import RedisCacheNonceStorage
 
 class TestCacheNonceStorage(TestCase):
     def setUp(self):
-        self.test_user = 'user'
+        self.test_user = 'bob'
         self.cache = caches['default']
         self.storage = RedisCacheNonceStorage()
 
     def test_store_and_update(self):
-        self.storage.store_nonce(self.test_user, 'abc', '')
-        self.assertEqual(self.cache.get(f'user_nonce_{self.test_user}_abc'), '')
+        self.storage.store_nonce(self.test_user, 'testnonce', '')
+        self.assertEqual(self.cache.get(f'user_nonce_{self.test_user}_testnonce'), '')
 
         # Should return true if the user + nonce already exists
-        self.assertTrue(self.storage.update_existing_nonce(self.test_user, 'abc', None))
+        self.assertTrue(self.storage.update_existing_nonce(self.test_user, 'testnonce', None))
 
-        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'ab', None))
-        self.assertFalse(self.storage.update_existing_nonce('someone', 'abc', None))
+        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'bogusnonce', None))
+        self.assertFalse(self.storage.update_existing_nonce('alice', 'testnonce', None))
 
         self.cache.clear()
-        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'abc', None))
+        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'testnonce', None))
         # update should never create
-        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'abc', None))
+        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'testnonce', None))
 
     def test_update_count(self):
-        self.storage.store_nonce(self.test_user, 'abc', 2)
+        self.storage.store_nonce(self.test_user, 'testnonce', 2)
 
-        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'abc', 2))
-        self.assertTrue(self.storage.update_existing_nonce(self.test_user, 'abc', 3))
+        self.assertFalse(self.storage.update_existing_nonce(self.test_user, 'testnonce', 2))
+        self.assertTrue(self.storage.update_existing_nonce(self.test_user, 'testnonce', 3))
     
     def xtest_nonce_lock(self):
         """
         Prove the lock halts execution, intended to be manually run
         """
-        nonce = 'abc'
+        nonce = 'testnonce'
         self.storage.store_nonce(self.test_user, nonce, 1)
         with self.cache.lock(f'user_nonce_lock_{self.test_user}_{nonce}'):
             self.storage.update_existing_nonce(self.test_user, nonce, 2)
