f740202660d6a2cc14b647c5a357cb39f1f736a9
Olivier Leger <olivierleger@gmail.com>
2022-08-23 15:06:08 -0400

    Fix migration conflicts with beta

--- onadata/apps/logger/migrations/0024_add_xform_attachment_storage_bytes.py
+++ onadata/apps/logger/migrations/0024_add_xform_attachment_storage_bytes.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('logger', '0022_misc_model_options'),
+        ('logger', '0023_alter_parsedinstance_id'),
     ]
 
     operations = [
