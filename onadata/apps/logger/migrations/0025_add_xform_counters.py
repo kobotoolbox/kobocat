f22ef34a041678e86ca1fb6d4724eadb0700b192
Olivier Leger <olivierleger@gmail.com>
2022-09-01 19:06:00 -0400

    Fix migration conflicts

--- onadata/apps/logger/migrations/0025_add_xform_counters.py
+++ onadata/apps/logger/migrations/0025_add_xform_counters.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('logger', '0022_misc_model_options'),
+        ('logger', '0024_add_xform_attachment_storage_bytes'),
     ]
 
     operations = [
--- onadata/apps/logger/migrations/0026_delete_submissioncounter.py
+++ onadata/apps/logger/migrations/0026_delete_submissioncounter.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('logger', '0023_add_xform_counters'),
+        ('logger', '0025_add_xform_counters'),
     ]
 
     operations = [
