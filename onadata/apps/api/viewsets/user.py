f55d161ea88dc3e4a8964d78f1b7d7f36ee92859
Olivier Leger <olivierleger@gmail.com>
2023-03-09 11:20:06 -0500

    Restrict delete user access to service account user

--- onadata/apps/api/viewsets/user.py
+++ onadata/apps/api/viewsets/user.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, mixins, renderers
 
+from ..permissions import UserDeletePermission
+
 
 class UserViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
 
     lookup_field = 'username'
     queryset = get_user_model().objects.all()
-    permission_classes = []
+    permission_classes = [UserDeletePermission]
     renderer_classes = (renderers.JSONRenderer,)
