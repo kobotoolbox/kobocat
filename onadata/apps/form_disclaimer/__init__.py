28644a8bb60eb2a076bf81a5573e3de06d964868
Olivier Leger <olivierleger@gmail.com>
2023-08-14 18:23:09 -0400

    Removed unused variables as requested for PR#882

--- onadata/apps/form_disclaimer/__init__.py
+++ onadata/apps/form_disclaimer/__init__.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class FormDisclaimerAppConfig(AppConfig):
     name = 'onadata.apps.form_disclaimer'
     verbose_name = 'Form disclaimer'
-
-    def ready(self):
-        super().ready()
--- onadata/libs/utils/xml.py
+++ onadata/libs/utils/xml.py
@@ -111,12 +111,8 @@ class XMLFormWithDisclaimer:
         Will add only translations that match form translations.
         """
 
-        translation_nodes = []
-        languages = []
         for n in self._root_node.getElementsByTagName('itext')[0].childNodes:
             if n.nodeType == Node.ELEMENT_NODE and n.tagName == 'translation':
-                languages.append(n.getAttribute('lang'))
-                translation_nodes.append(n)
                 disclaimer_translation = self._root_node.createElement('text')
                 disclaimer_translation.setAttribute(
                     'id',
