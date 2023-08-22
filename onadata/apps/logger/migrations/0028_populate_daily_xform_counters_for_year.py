5ed9acc552814b26ec426730a0b5f797cf280210
John N. Milner <john@tmoj.net>
2023-08-22 17:28:45 -0400

    Revise migration comments
    * Clarify that affected endpoints are in KPI
    * Show management command instructions only when
      SKIP_HEAVY_MIGRATIONS=True

--- onadata/apps/logger/migrations/0028_populate_daily_xform_counters_for_year.py
+++ onadata/apps/logger/migrations/0028_populate_daily_xform_counters_for_year.py
@@ -8,11 +8,12 @@ def populate_daily_counts_for_year(apps, schema_editor):
         print(
             """
             !!! ATTENTION !!!
-            If you have existing projects you need to run this management command:
+            If you have existing projects, you need to run this management command:
 
                > python manage.py populate_submission_counters -f --skip_monthly
 
-            Until you do, total usage counts from /api/v2/service_usage and /api/v2/asset_usage will be incorrect
+            Until you do, total usage counts from the KPI endpoints
+            /api/v2/service_usage and /api/v2/asset_usage will be incorrect
             """
         )
     else:
@@ -20,10 +21,8 @@ def populate_daily_counts_for_year(apps, schema_editor):
             """
             This might take a while. If it is too slow, you may want to re-run the
             migration with SKIP_HEAVY_MIGRATIONS=True and run the following management command:
-            
-                           > python manage.py populate_submission_counters -f --skip_monthly
 
-            Until you do, total usage counts from /api/v2/service_usage and /api/v2/asset_usage will be incorrect
+                > python manage.py populate_submission_counters -f --skip_monthly
             """
         )
         call_command('populate_submission_counters', force=True, skip_monthly=True)
