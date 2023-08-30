7cb02a2470d8da2c0638da109f5a31b698940180
John N. Milner <john@tmoj.net>
2023-08-29 22:54:44 -0400

    Assert redirect location is correct; remove print

--- onadata/apps/main/tests/test_form_auth.py
+++ onadata/apps/main/tests/test_form_auth.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from django.conf import settings
 from django.urls import reverse
 
 from .test_base import TestBase
@@ -13,11 +14,17 @@ class TestFormAuth(TestBase):
         self._create_user_and_login(username='bob', password='bob')
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 302)
-        print("response['Location']", response['Location'], flush=True)
+        received, desired = (
+            u.rstrip('/') for u in (response['Location'], settings.KOBOFORM_URL)
+        )
+        assert received == desired
 
     def test_profile_redirects(self):
         self._create_user_and_login(username='bob', password='bob')
         url = reverse('user_profile', kwargs={'username': self.user.username})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        print("response['Location']", response['Location'], flush=True)
+        received, desired = (
+            u.rstrip('/') for u in (response['Location'], settings.KOBOFORM_URL)
+        )
+        assert received == desired
