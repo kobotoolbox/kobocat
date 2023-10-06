f90d0e021bedabada7e007471cb00d133fcf17b4
Olivier Leger <olivierleger@gmail.com>
2023-10-06 07:37:44 -0400

    Use previous migration date for lower bound range

--- onadata/apps/logger/migrations/0030_backfill_lost_monthly_counters.py
+++ onadata/apps/logger/migrations/0030_backfill_lost_monthly_counters.py
@@ -1,4 +1,5 @@
 from django.db import migrations
+from django.db.migrations.recorder import MigrationRecorder
 from django.db.models import Sum
 from django.db.models import Value, F, DateField
 from django.db.models.functions import Cast, Concat
@@ -11,13 +12,14 @@ def populate_missing_monthly_counters(apps, schema_editor):
     DailyXFormSubmissionCounter = apps.get_model('logger', 'DailyXFormSubmissionCounter')  # noqa
     MonthlyXFormSubmissionCounter = apps.get_model('logger', 'MonthlyXFormSubmissionCounter')  # noqa
 
-    first_daily_counter = DailyXFormSubmissionCounter.objects.order_by(
-        'date'
-    ).first()
-
-    if not first_daily_counter:
+    if not DailyXFormSubmissionCounter.objects.all().exists():
         return
 
+    previous_migration = MigrationRecorder.Migration.objects.filter(
+        app='logger', name='0029_populate_daily_xform_counters_for_year'
+    ).first()
+
+    # Delete monthly counters in the range if any (to avoid conflicts in bulk_create below)
     MonthlyXFormSubmissionCounter.objects.annotate(
         date=Cast(
             Concat(
@@ -25,11 +27,14 @@ def populate_missing_monthly_counters(apps, schema_editor):
             ),
             DateField(),
         )
-    ).filter(date__gte=first_daily_counter.date.replace(day=1)).delete()
+    ).filter(date__gte=previous_migration.applied.date().replace(day=1)).delete()
 
     records = (
         DailyXFormSubmissionCounter.objects.filter(
-            date__range=[first_daily_counter.date.replace(day=1), now().date()]
+            date__range=[
+                previous_migration.applied.date().replace(day=1),
+                now().date()
+            ]
         )
         .annotate(year=ExtractYear('date'), month=ExtractMonth('date'))
         .values('month', 'year')
@@ -39,7 +44,7 @@ def populate_missing_monthly_counters(apps, schema_editor):
 
     # Do not use `ignore_conflicts=True` to ensure all counters are successfully
     # create.
-    # TODO use `update_conflicts` with Django 4.2
+    # TODO use `update_conflicts` with Django 4.2 and avoid `.delete()` above
     MonthlyXFormSubmissionCounter.objects.bulk_create(
         [
             MonthlyXFormSubmissionCounter(
