57040f15516176a371a2228c89540f05ff675322
Olivier Leger <olivierleger@gmail.com>
2023-10-19 14:56:07 -0400

    Avoid not edited instance

--- onadata/apps/logger/management/commands/soft_delete_orphan_attachments.py
+++ onadata/apps/logger/management/commands/soft_delete_orphan_attachments.py
@@ -8,6 +8,7 @@ from onadata.apps.logger.models import (
 )
 from onadata.apps.logger.signals import pre_delete_attachment
 from onadata.libs.utils.logger_tools import get_soft_deleted_attachments
+from onadata.apps.viewer.models.parsed_instance import datetime_from_str
 
 
 class Command(BaseCommand):
@@ -19,14 +20,33 @@ class Command(BaseCommand):
             '--chunks',
             type=int,
             default=2000,
-            help="Number of records to process per query"
+            help='Number of records to process per query'
+        )
+
+        parser.add_argument(
+            '--start-id',
+            type=int,
+            default=0,
+            help='Instance ID to start from'
+        )
+
+        parser.add_argument(
+            '--start-date',
+            type=str,
+            default=None,
+            help='Starting date to start from. Format: yyyy-mm-aa.'
         )
 
     def handle(self, *args, **kwargs):
         chunks = kwargs['chunks']
         verbosity = kwargs['verbosity']
+        start_id = kwargs['start_id']
+        start_date = kwargs['start_date']
 
         queryset = Attachment.objects.values_list('instance_id', flat=True).distinct()
+
+        if start_id:
+            queryset = queryset.filter(instance_id__gte=start_id)
 
         if verbosity > 1:
             self.stdout.write(
@@ -43,11 +63,58 @@ class Command(BaseCommand):
                 self.stdout.write(
                     f'Processing Instance object #{instance_id}{message}…'
                 )
-            soft_deleted_attachments = get_soft_deleted_attachments(instance)
+
+            if start_date and instance.date_created < datetime_from_str(
+                f'{start_date}T00:00:00 +0000'
+            ):
+                if verbosity > 1:
+                    message = '' if verbosity <= 1 else f' - {cpt}/{instances_count}'
+                    self.stdout.write(
+                        f'\tSkip Instance object #{instance_id}{message}. Too old'
+                    )
+                cpt += 1
+                continue
+
+            if not self._has_instance_been_edited(instance):
+                if verbosity > 1:
+                    message = '' if verbosity <= 1 else f' - {cpt}/{instances_count}'
+                    self.stdout.write(
+                        f'\tSkip Instance object #{instance_id}{message}. Not edited'
+                    )
+                cpt += 1
+                continue
+
+            try:
+                soft_deleted_attachments = get_soft_deleted_attachments(instance)
+            except Exception as e:
+                cpt += 1
+                if verbosity > 0:
+                    self.stderr.write(
+                        f'\tError for Instance object #{instance_id}: {str(e)}'
+                    )
+                continue
+
             for soft_deleted_attachment in soft_deleted_attachments:
                 pre_delete_attachment(
                     soft_deleted_attachment, only_update_counters=True
                 )
+            if verbosity > 1:
+                message = '' if verbosity <= 1 else f' - {cpt}/{instances_count}'
+                self.stdout.write(
+                    f'\tInstance object #{instance_id}{message} updated!'
+                )
+            cpt += 1
 
             cpt += 1
         self.stdout.write('Done!')
+
+    def _has_instance_been_edited(self, instance):
+        """
+        Consider instance as edited if it modification date is more or less 10
+        seconds apart
+        """
+        date_created_ts = instance.date_created.timestamp()
+        date_modified_ts = instance.date_modified.timestamp()
+        return not (
+            date_created_ts <= date_modified_ts <= date_created_ts + 10
+        )
