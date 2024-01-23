68726052d58c3457cff3483f8f06560711494c38
David Burke <david@burkesoftware.com>
2024-01-23 12:02:20 -0500

    test_site_name overrides KOBOCAT_PUBLIC_HOSTNAME in case it was changed from it's default setting

--- onadata/apps/main/tests/test_custom_context_processors.py
+++ onadata/apps/main/tests/test_custom_context_processors.py
@@ -1,9 +1,10 @@
 # coding: utf-8
-from django.test import TestCase
+from django.test import override_settings, TestCase
 from onadata.apps.main.context_processors import site_name
 
 
 class CustomContextProcessorsTest(TestCase):
+    @override_settings(KOBOCAT_PUBLIC_HOSTNAME="kc.kobotoolbox.org")
     def test_site_name(self):
         context = site_name(None)
         self.assertEqual(context, {'SITE_NAME': 'kc.kobotoolbox.org'})
