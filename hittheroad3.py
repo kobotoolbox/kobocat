6440380a2c3e39e8f51bea16262310dd1cb3516c
John N. Milner <john@tmoj.net>
2024-02-18 16:03:07 -0500

    Hard-code different stuff and restore some logging

--- hittheroad3.py
+++ hittheroad3.py
@@ -35,13 +35,12 @@ from onadata.apps.logger.models.survey_type import SurveyType
 # to be replaced by reading usernames from a file
 # all_users_qs = User.objects.filter(username__in=('tino', 'tinok', 'tinok3', 'jamesld_test'))
 
-usernames = [x.strip() for x in open('../kf-usernames.txt').readlines()]
+usernames = [x.strip() for x in open('../eu-usernames.txt').readlines()]
 all_users_qs = User.objects.filter(username__in=usernames)
-# useless if we're running multiple processes in parallel
-# csv_file_writer = csv.writer(open('/home/ubuntu/jnm-work/log/kf-kc.log', 'w'))
+csv_file_writer = csv.writer(open('/home/ubuntu/jnm-work/log/eu-kc-3.log', 'w'))
 
 
-CHUNK_SIZE = 500
+CHUNK_SIZE = 1000
 
 counts = defaultdict(lambda: 1)
 
