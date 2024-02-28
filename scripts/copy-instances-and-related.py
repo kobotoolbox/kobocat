deaf51cce2a843ad825d59e280800f2788694cc5
John N. Milner <john@tmoj.net>
2024-02-27 19:24:50 -0500

    Reorder functions

--- scripts/copy-instances-and-related.py
+++ scripts/copy-instances-and-related.py
@@ -171,6 +171,75 @@ def copy_related_objs(
                 ] = created_obj.pk
 
 
+def copy_related_obj(
+    obj,
+    related_fk_field,
+    nat_key: list = None,
+    retain_pk=False,
+    fixup: callable = None,
+):
+    # Yay, code duplication
+    model = type(obj)
+    disable_auto_now(model)  # never reenable within this single-use process
+    related_field = model._meta.get_field(related_fk_field)
+    related_id_attr = related_field.attname
+    related_model = related_field.related_model
+    try:
+        related_source_to_dest_pks = source_to_dest_pks[related_model]
+    except KeyError:
+        raise Exception(
+            f'{related_model} must be copied with `nat_key` specified before'
+            f' {model} can be copied'
+        )
+    this_model_source_to_dest_pks = source_to_dest_pks.setdefault(model, {})
+    source_obj_pk = obj.pk
+    if not retain_pk:
+        obj.pk = None
+    source_related_id = getattr(obj, related_id_attr)
+    try:
+        dest_related_id = related_source_to_dest_pks[source_related_id]
+    except KeyError:
+        raise Exception(
+            f'{legible_class(model)} #{source_obj_pk} expects {related_model}'
+            f' #{source_related_id}, but it has not been copied'
+        )
+    setattr(obj, related_id_attr, dest_related_id)
+    if fixup:
+        # We don't have all the time in the world… apply any necessary
+        # hacks here
+        try:
+            fixup(obj)
+        except SkipObject:
+            return obj
+    status = 'done'
+    if not nat_key:
+        with route_to_dest():
+            model.objects.bulk_create([obj])
+    else:
+        nat_key_vals = tuple(getattr(obj, f) for f in nat_key)
+        with route_to_dest():
+            criteria = dict(zip(nat_key, nat_key_vals))
+            criteria[related_id_attr] = dest_related_id
+            try:
+                existing_obj = model.objects.get(**criteria)
+            except model.DoesNotExist:
+                # "bulk create" single item to sidestep `save()` logic
+                model.objects.bulk_create([obj])
+            else:
+                obj.pk = existing_obj.pk
+                status ='already exists!'
+        this_model_source_to_dest_pks[source_obj_pk] = obj.pk
+    print_csv(
+        f'✅ {legible_class(model)}',
+        source_obj_pk,
+        obj.pk,
+        status,
+        f'(complete: {counts[model]})',
+    )
+    counts[model] += 1
+    return obj
+
+
 def run(username):
     print(f'Username {username}')
 
@@ -263,70 +332,3 @@ def run(username):
     )
 
 
-def copy_related_obj(
-    obj,
-    related_fk_field,
-    nat_key: list = None,
-    retain_pk=False,
-    fixup: callable = None,
-):
-    # Yay, code duplication
-    model = type(obj)
-    disable_auto_now(model)  # never reenable within this single-use process
-    related_field = model._meta.get_field(related_fk_field)
-    related_id_attr = related_field.attname
-    related_model = related_field.related_model
-    try:
-        related_source_to_dest_pks = source_to_dest_pks[related_model]
-    except KeyError:
-        raise Exception(
-            f'{related_model} must be copied with `nat_key` specified before'
-            f' {model} can be copied'
-        )
-    this_model_source_to_dest_pks = source_to_dest_pks.setdefault(model, {})
-    source_obj_pk = obj.pk
-    if not retain_pk:
-        obj.pk = None
-    source_related_id = getattr(obj, related_id_attr)
-    try:
-        dest_related_id = related_source_to_dest_pks[source_related_id]
-    except KeyError:
-        raise Exception(
-            f'{legible_class(model)} #{source_obj_pk} expects {related_model}'
-            f' #{source_related_id}, but it has not been copied'
-        )
-    setattr(obj, related_id_attr, dest_related_id)
-    if fixup:
-        # We don't have all the time in the world… apply any necessary
-        # hacks here
-        try:
-            fixup(obj)
-        except SkipObject:
-            return obj
-    status = 'done'
-    if not nat_key:
-        with route_to_dest():
-            model.objects.bulk_create([obj])
-    else:
-        nat_key_vals = tuple(getattr(obj, f) for f in nat_key)
-        with route_to_dest():
-            criteria = dict(zip(nat_key, nat_key_vals))
-            criteria[related_id_attr] = dest_related_id
-            try:
-                existing_obj = model.objects.get(**criteria)
-            except model.DoesNotExist:
-                # "bulk create" single item to sidestep `save()` logic
-                model.objects.bulk_create([obj])
-            else:
-                obj.pk = existing_obj.pk
-                status ='already exists!'
-        this_model_source_to_dest_pks[source_obj_pk] = obj.pk
-    print_csv(
-        f'✅ {legible_class(model)}',
-        source_obj_pk,
-        obj.pk,
-        status,
-        f'(complete: {counts[model]})',
-    )
-    counts[model] += 1
-    return obj
